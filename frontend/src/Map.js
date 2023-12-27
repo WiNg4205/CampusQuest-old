2416a1e2e62d542efde6435bc2fc65eab8415d0e
Michael Tanto <michaeltanto0909@gmail.com>
2023-12-28 01:20:23 +0700

    added location data

--- frontend/src/Map.js
+++ frontend/src/Map.js
@@ -1,21 +1,55 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Map() {
+
   useEffect(() => {
     const script = document.createElement('script');
     script.src = 'https://api.mazemap.com/js/v2.0.114/mazemap.min.js';
     script.async = true;
-
+    
     document.head.appendChild(script);
+
+    const lngLat = {lng: 151.2300, lat: -33.9172};
+    const campusId = 111;
+    const zLevel = 3;
 
     script.onload = () => {
       const map = new window.Mazemap.Map({
         container: 'map',
-        campuses: 111,
-        center: {lng: 151.2300, lat: -33.9172},
+        campuses: campusId,
+        center: lngLat,
         zoom: 16,
-        zLevel: 3
+        zLevel: zLevel
       });
+
+      // get user's location data
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            var locationData = {lng: position.coords.longitude, lat: position.coords.latitude};
+
+            // add blue dot to map
+            map.on('load', function() {
+              window.blueDot = new window.Mazemap.BlueDot({
+                map: map,
+              })
+                .setZLevel(zLevel)
+                .setAccuracy(10)
+                .setLngLat(locationData)
+                .setBearingAccuracy(10)
+                .setBearing(0)
+                .showBearingHint()
+                .show();
+            })
+          },
+          (error) => {
+            console.error('Error getting user location:', error);
+          }
+        );
+      }
+      else {
+        console.error('Geolocation is not supported by this browser.');
+      }
 
       return () => {
         map.destroy();
