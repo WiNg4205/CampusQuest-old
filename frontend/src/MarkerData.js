b0f8c1380d6d8dbccf6f6157b493d0c94def5c64
tetnaylin <tetnaylin168@gmail.com>
2024-01-04 19:55:14 +1100

    Adding restaurant and cafe markers

--- frontend/src/MarkerData.js
+++ frontend/src/MarkerData.js
@@ -1,69 +1,356 @@
+// Total: 31 markers
+
 var markerData = [
-  { 
-    lngLat: {lng: 151.225817, lat: -33.9153836},
+  ////////////////////////////////////////////////////////////////////////////////////////
+  /////////////// All Cafes (Currently 10 cafes on 4/1/2024) Colour: Purple //////////////
+  ////////////////////////////////////////////////////////////////////////////////////////
+  { // Poolside Café
+    lngLat: {lng: 151.226500, lat: -33.915281},
     options: {
-      imgUrl: 'gyg-logo.png',
-      color: 'MazeRed',
+      imgUrl: 'insert-logo.png',
+      imgScale: 1,
+      color: 'MazePurple',
       size: 50,
-      innerCircle: true,
-      innerCircleColor: 'white',
-      innerCircleScale: 0.7,
       shape: 'marker',
       zLevel: 1
     }
   },
-  {
-    lngLat: {lng: 151.2309757, lat: -33.916822},
+  { // Maze Cafe
+    lngLat: {lng: 151.228154, lat: -33.915413},
     options: {
-      imgUrl: 'caffe-brioso-logo.png',
-      imgScale: 2,
+      imgUrl: 'insert-logo.png',
+      imgScale: 1,
       color: 'MazePurple',
       size: 50,
-      innerCircle: false,
-      innerCircleScale: 0.6,
       shape: 'marker',
       zLevel: 1
     }
   },
-  {
-    lngLat: {lng: 151.2305189, lat: -33.9168135},
+  { // Plume Café
+    lngLat: {lng: 151.230234, lat: -33.916928},
     options: {
-      imgUrl: 'boost-juice-logo.png',
-      imgScale: 2,
+      imgUrl: 'insert-logo.png',
+      imgScale: 1,
       color: 'MazePurple',
       size: 50,
-      innerCircle: false,
-      innerCircleScale: 0.6,
       shape: 'marker',
       zLevel: 1
     }
   },
-  {
-    lngLat: {lng: 151.2258017, lat: -33.916},
+  { // Home Ground Kiosk
+    lngLat: {lng: 151.228624, lat: -33.918015},
     options: {
-      imgUrl: 'yallah-eat-logo.png',
-      imgScale: 0.8,
+      imgUrl: 'insert-logo.png',
+      imgScale: 1,
       color: 'MazePurple',
       size: 50,
-      innerCircle: false,
-      innerCircleScale: 0.6,
       shape: 'marker',
       zLevel: 1
     }
   },
-  {
-    lngLat: {lng: 151.2272977, lat: -33.9166969},
+  { // Coffee on Campus Cafe
+    lngLat: {lng: 151.231146, lat: -33.918394},
     options: {
-      imgUrl: 'nene-chicken-logo.png',
-      imgScale: 3,
+      imgUrl: 'insert-logo.png',
+      imgScale: 1,
       color: 'MazePurple',
       size: 50,
-      innerCircle: false,
-      innerCircleScale: 0.6,
+      shape: 'marker',
+      zLevel: 1
+    }
+  },
+  { // The Lounge
+    lngLat: {lng: 151.233379, lat: -33.917687},
+    options: {
+      imgUrl: 'insert-logo.png',
+      imgScale: 1,
+      color: 'MazePurple',
+      size: 50,
+      shape: 'marker',
+      zLevel: 1
+    }
+  },
+  { // Caffé Brioso
+    lngLat: {lng: 151.233999, lat: -33.917120},
+    options: {
+      imgUrl: 'insert-logo.png',
+      imgScale: 1,
+      color: 'MazePurple',
+      size: 50,
+      shape: 'marker',
+      zLevel: 1
+    }
+  },
+  { // The Little Marionette Cafe
+    lngLat: {lng: 151.234522, lat: -33.917191},
+    options: {
+      imgUrl: 'insert-logo.png',
+      imgScale: 1,
+      color: 'MazePurple',
+      size: 50,
+      shape: 'marker',
+      zLevel: 1
+    }
+  },
+  { // Gradueat
+    lngLat: {lng: 151.234656, lat: -33.917243},
+    options: {
+      imgUrl: 'insert-logo.png',
+      imgScale: 1,
+      color: 'MazePurple',
+      size: 50,
+      shape: 'marker',
+      zLevel: 1
+    }
+  },
+  { // XS Espresso
+    lngLat: {lng: 151.235261, lat: -33.917319},
+    options: {
+      imgUrl: 'insert-logo.png',
+      imgScale: 1,
+      color: 'MazePurple',
+      size: 50,
+      shape: 'marker',
+      zLevel: 1
+    }
+  },
+  ////////////////////////////////////////////////////////////////////////////////////////
+
+  ////////////////////////////////////////////////////////////////////////////////////////
+  ///////// All Restaurants (Currently 21 restaurants on 4/1/2024) Colour: Green /////////
+  ////////////////////////////////////////////////////////////////////////////////////////
+  { // Guzman Y Gomez
+    lngLat: {lng: 151.227996, lat: -33.915423},
+    options: {
+      imgUrl: 'insert-logo.png',
+      imgScale: 1,
+      color: 'MazeGreen',
+      size: 50,
+      shape: 'marker',
+      zLevel: 1
+    }
+  },
+  { // G02
+    lngLat: {lng: 151.227732, lat: -33.915427},
+    options: {
+      imgUrl: 'insert-logo.png',
+      imgScale: 1,
+      color: 'MazeGreen',
+      size: 50,
+      shape: 'marker',
+      zLevel: 1
+    }
+  },
+  { // Mamak Village
+    lngLat: {lng: 151.227713, lat: -33.915533},
+    options: {
+      imgUrl: 'insert-logo.png',
+      imgScale: 1,
+      color: 'MazeGreen',
+      size: 50,
+      shape: 'marker',
+      zLevel: 1
+    }
+  },
+  { // Yalla Eat
+    lngLat: {lng: 151.227932, lat: -33.915543},
+    options: {
+      imgUrl: 'insert-logo.png',
+      imgScale: 1,
+      color: 'MazeGreen',
+      size: 50,
+      shape: 'marker',
+      zLevel: 1
+    }
+  },
+  { // IGA UNSW
+    lngLat: {lng: 151.228100, lat: -33.915522},
+    options: {
+      imgUrl: 'insert-logo.png',
+      imgScale: 1,
+      color: 'MazeGreen',
+      size: 50,
+      shape: 'marker',
+      zLevel: 1
+    }
+  },
+  { // ShareTea
+    lngLat: {lng: 151.227803, lat: -33.915670},
+    options: {
+      imgUrl: 'insert-logo.png',
+      imgScale: 1,
+      color: 'MazeGreen',
+      size: 50,
+      shape: 'marker',
+      zLevel: 1
+    }
+  },
+  { // Stellini Pasta
+    lngLat: {lng: 151.227875, lat: -33.915676},
+    options: {
+      imgUrl: 'insert-logo.png',
+      imgScale: 1,
+      color: 'MazeGreen',
+      size: 50,
+      shape: 'marker',
+      zLevel: 1
+    }
+  },
+  { // Unibar & Thirsty Burger
+    lngLat: {lng: 151.226991, lat: -33.916650},
+    options: {
+      imgUrl: 'insert-logo.png',
+      imgScale: 1,
+      color: 'MazeGreen',
+      size: 50,
+      shape: 'marker',
+      zLevel: 1
+    }
+  },
+  { // Thoughtful Foods
+    lngLat: {lng: 151.227190, lat: -33.917035},
+    options: {
+      imgUrl: 'insert-logo.png',
+      imgScale: 1,
+      color: 'MazeGreen',
+      size: 50,
+      shape: 'marker',
+      zLevel: 1
+    }
+  },
+  { // Quad Food Court
+    lngLat: {lng: 151.230471, lat: -33.917294},
+    options: {
+      imgUrl: 'insert-logo.png',
+      imgScale: 1,
+      color: 'MazeGreen',
+      size: 50,
+      shape: 'marker',
+      zLevel: 1
+    }
+  },
+  { // Papa Rich
+    lngLat: {lng: 151.230601, lat: -33.917279},
+    options: {
+      imgUrl: 'insert-logo.png',
+      imgScale: 1,
+      color: 'MazeGreen',
+      size: 50,
+      shape: 'marker',
+      zLevel: 1
+    }
+  },
+  { // Nene Chicken
+    lngLat: {lng: 151.230602, lat: -33.917212},
+    options: {
+      imgUrl: 'insert-logo.png',
+      imgScale: 1,
+      color: 'MazeGreen',
+      size: 50,
+      shape: 'marker',
+      zLevel: 1
+    }
+  },
+  { // Soul Origin
+    lngLat: {lng: 151.230582, lat: -33.917180},
+    options: {
+      imgUrl: 'insert-logo.png',
+      imgScale: 1,
+      color: 'MazeGreen',
+      size: 50,
+      shape: 'marker',
+      zLevel: 1
+    }
+  },
+  { // Subway
+    lngLat: {lng: 151.232677, lat: -33.916789},
+    options: {
+      imgUrl: 'insert-logo.png',
+      imgScale: 1,
+      color: 'MazeGreen',
+      size: 50,
+      shape: 'marker',
+      zLevel: 1
+    }
+  },
+  { // Boost Juice
+    lngLat: {lng: 151.232665, lat: -33.916847},
+    options: {
+      imgUrl: 'insert-logo.png',
+      imgScale: 1,
+      color: 'MazeGreen',
+      size: 50,
+      shape: 'marker',
+      zLevel: 1
+    }
+  },
+  { // Southern Wok
+    lngLat: {lng: 151.232920, lat: -33.916799},
+    options: {
+      imgUrl: 'insert-logo.png',
+      imgScale: 1,
+      color: 'MazeGreen',
+      size: 50,
+      shape: 'marker',
+      zLevel: 1
+    }
+  },
+  { // Laksa Delight 
+    lngLat: {lng: 151.234265, lat: -33.917287},
+    options: {
+      imgUrl: 'insert-logo.png',
+      imgScale: 1,
+      color: 'MazeGreen',
+      size: 50,
+      shape: 'marker',
+      zLevel: 1
+    }
+  },
+  { // Classic Kebab
+    lngLat: {lng: 151.234289, lat: -33.917144},
+    options: {
+      imgUrl: 'insert-logo.png',
+      imgScale: 1,
+      color: 'MazeGreen',
+      size: 50,
+      shape: 'marker',
+      zLevel: 1
+    }
+  },
+  { // Sushi Roll
+    lngLat: {lng: 151.234413, lat: -33.917315},
+    options: {
+      imgUrl: 'insert-logo.png',
+      imgScale: 1,
+      color: 'MazeGreen',
+      size: 50,
+      shape: 'marker',
+      zLevel: 1
+    }
+  },
+  { // Stockmarket
+    lngLat: {lng: 151.234584, lat: -33.917358},
+    options: {
+      imgUrl: 'insert-logo.png',
+      imgScale: 1,
+      color: 'MazeGreen',
+      size: 50,
+      shape: 'marker',
+      zLevel: 1
+    }
+  },
+  { // Tropical Green (Pho House)
+    lngLat: {lng: 151.234409, lat: -33.917617},
+    options: {
+      imgUrl: 'insert-logo.png',
+      imgScale: 1,
+      color: 'MazeGreen',
+      size: 50,
       shape: 'marker',
       zLevel: 1
     }
   }
+  ////////////////////////////////////////////////////////////////////////////////////////
 ];
 
 export default markerData;
